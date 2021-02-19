25f44d0cd74bcb0ed924e54706ba1b19e0ba4465
JoTerrance <gorka.sanz@salesianos.edu>
2021-02-19 13:47:52 +0100

    almost working example

---
+++ src/main/java/org/javacodegeeks/webservices/soap/surcharge/generated/package-info.java
@@ -0,0 +1,9 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.3.2 
+// Visite <a href="https://javaee.github.io/jaxb-v2/">https://javaee.github.io/jaxb-v2/</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.02.19 a las 01:28:16 PM CET 
+//
+
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.javacodegeeks.org/webservices/soap/surcharge/generated", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.javacodegeeks.webservices.soap.surcharge.generated;
